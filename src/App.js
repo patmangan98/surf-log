6695a19f9039acf14cc469d33930aa6c0cb31af6
Damaris Torrent <damaristorrent@gmail.com>
2023-08-07 12:25:12 -0400

    parsing text file

--- src/App.js
+++ src/App.js
@@ -2,12 +2,39 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  
+    const fileUrl = '/view_text_file.php?filename=4100452023.txt.gz&dir=data/stdmet/May/'
+    // console.log(fileUrl)
+    fetch(fileUrl)
+    // fetch(fileUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.text();
+    })
+    .then(fileContent => {
+      // Process the file content here
+     
+      const dataString = fileContent.split('\n')
+      console.log(dataString)
+     
+      // You can save the file content to a variable or use it as needed
+      // For example, you can pass it to another function for further processing
+      // yourFunctionToProcessFileContent(fileContent);
+    })
+    .catch(error => {
+      console.error('Error reading the file:', error);
+    });
+     
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello from SurfLog
         </p>
         <a
           className="App-link"
