9f287f7dc3d97ea94bd7cb1ac276ba894bedc67d
Damaris Torrent <damaristorrent@gmail.com>
2023-08-17 16:03:28 -0400

    Added dependency array to useFetch

--- front-end/src/pages/app/App.js
+++ front-end/src/pages/app/App.js
@@ -2,81 +2,65 @@ import "./App.css"
 import React, { useState, useEffect } from "react"
 
 function App() {
- 
-  let dataSplit = []
   const [message, setMessage] = useState("")
-  
+
   const [currentReading, setCurrentReading] = useState({
     day: "",
     month: "",
     year: "",
     hour: "",
     minute: ""
-  });
+  })
 
   useEffect(() => {
     fetch("http://localhost:8000/message")
       .then((res) => res.json())
       .then((data) => setMessage(data.message))
-  }, [])
-
-  const fileUrl =
-    "data/realtime2/41004.txt"
-  
-  fetch(fileUrl)
-    // fetch(fileUrl)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok")
-      }
-      return response.text()
-    })
-    .then((fileContent) => {
-      // Process the file content here
-
-      const dataString = fileContent.split("\n")
-      
-      dataSplit = dataString[2].split(" ")
-      
-      // setCurrentReading({
-      //   ...currentReading,
-      //   day: dataSplit[2],
-      //   month: dataSplit[1],
-      //   year: dataSplit[0],
-      //   hour: dataSplit[3],
-      //   minute: dataSplit[4]
-      // })
-
-      const [year, month, day, hour, minute] = dataSplit
-
-      setCurrentReading({
-        day,
-        month,
-        year,
-        hour,
-        minute
+      .catch((error) => {
+        console.error("Error fetching message:", error)
       })
 
-      // You can save the file content to a variable or use it as needed
-      // For example, you can pass it to another function for further processing
-      // yourFunctionToProcessFileContent(fileContent);
-    })
-    .catch((error) => {
-      console.error("Error reading the file:", error)
-    })
+    const fileUrl = "data/realtime2/41004.txt"
 
-   
+    fetch(fileUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok")
+        }
+        return response.text()
+      })
+      .then((fileContent) => {
+        const dataString = fileContent.split("\n")
+        const dataSplit = dataString[2].split(" ")
+        const [year, month, day, hour, minute] = dataSplit
+
+        setCurrentReading({
+          day,
+          month,
+          year,
+          hour,
+          minute
+        })
+      })
+      .catch((error) => {
+        console.error("Error reading the file:", error)
+      })
+  }, [message])
+  console.log(message)
+  console.log(currentReading.month)
 
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>Hello from SurfLog</p>
-        <p>This is a message from the back end Express server: {message}</p>
-        <p>The last buoy reading was at: {currentReading.month}-{currentReading.day}-{currentReading.year}:{currentReading.hour}:{currentReading.minute} GMT time.</p>
+        <p>This is a message from the backend Express server: {message}</p>
+        <p>
+          The last buoy reading was at: {currentReading.month}-{currentReading.day}-{currentReading.year}:{currentReading.hour}:{currentReading.minute} GMT time.
+        </p>
       </header>
     </div>
   )
 }
 
 export default App
+
