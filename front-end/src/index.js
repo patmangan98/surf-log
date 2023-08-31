17efc6f7f405e28b641105572ea6a9ebb618a3e9
patmangan98 <pat.mangan@outlook.com>
2023-08-31 19:46:10 -0400

    Completed styling for login form

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -4,6 +4,10 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css';
 import App from './pages/app/App'
 import reportWebVitals from './reportWebVitals';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
