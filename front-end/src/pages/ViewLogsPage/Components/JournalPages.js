c8a6d8eb92cb0dc188b48418a8ae5bf58b83aa10
patmangan98 <pat.mangan@outlook.com>
2023-10-14 19:20:36 -0400

    comming to a stopping point for today. The basic design is laid out. All that needs to be added is the carousel and the hieght spacing?

--- front-end/src/pages/ViewLogsPage/Components/JournalPages.js
+++ front-end/src/pages/ViewLogsPage/Components/JournalPages.js
@@ -4,16 +4,29 @@ import StickyNotes from "./StickyNotes"
 export default function JournalPages() {
     return (
         <>
-            <Grid container>
-                <Card>
-                    <Typography>This is the left card</Typography>
-                    <StickyNotes></StickyNotes>
-                </Card>
-
-                <Card>
-                    <Typography>This is the right card</Typography>
-                </Card>
-
+            <Grid
+                container
+                spacing={2}
+                justifyContent={'center'}
+                alignContent={'center'}
+                sx={{
+                    maxWidth: '100vw',
+                    height: '80vh'
+                }} >
+                <Grid item 
+                xs={5}
+                sx ={{maxHeight: '50vh'}}
+                >
+                    <Card>
+                        <Typography>This is the left card</Typography>
+                        <StickyNotes></StickyNotes>
+                    </Card>
+                </Grid>
+                <Grid item xs={5}>
+                    <Card>
+                        <Typography>This is the right card</Typography>
+                    </Card>
+                </Grid>
 
             </Grid>
         </>
