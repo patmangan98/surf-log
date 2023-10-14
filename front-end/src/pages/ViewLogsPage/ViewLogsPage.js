eb0f4e3cdb6f9138d6a1b123176bf4c8cca164a6
patmangan98 <pat.mangan@outlook.com>
2023-10-14 17:52:36 -0400

    Laying out the next portion of how I want to be able to render everything

--- front-end/src/pages/ViewLogsPage/ViewLogsPage.js
+++ front-end/src/pages/ViewLogsPage/ViewLogsPage.js
@@ -20,6 +20,16 @@ for(let i = 0; i < posts.length; i++) {
     console.log(posts[`${i}`])
 }
 
+// to do: make a sticky note component that can be easily mapped over 
+
+// put that in the left div, and the actual text into the right div 
+
+// wrap those divs in the carousel 
+
+//style properly 
+
+
+
 
     return (
         <>
