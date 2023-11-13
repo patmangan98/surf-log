fbd5114c1171de504c624223aea7572797279557
patmangan98 <pat.mangan@outlook.com>
2023-11-12 20:23:45 -0500

    Working on fixing some styling issues on the view log page

--- front-end/src/pages/ViewLogsPage/ViewLogsPage.js
+++ front-end/src/pages/ViewLogsPage/ViewLogsPage.js
@@ -7,28 +7,6 @@ import JournalPages from "./Components/JournalPages"
 
 export default function ViewLogsPage () {
 
-// const [posts, setPosts] = useState([])
-
-// useEffect(() => {
-//     indexPosts(getUserId())
-//         .then(res => res.json())
-//         .then(resData => setPosts(resData))
-// }, [])
-
-// // console.log(posts)
-
-
-// for(let i = 0; i < posts.length; i++) {
-//     console.log(posts[`${i}`])
-// }
-
-// to do: make a sticky note component that can be easily mapped over 
-
-// put that in the left div, and the actual text into the right div 
-
-// wrap those divs in the carousel 
-
-//style properly 
 return (
     <>
     <JournalPages />
